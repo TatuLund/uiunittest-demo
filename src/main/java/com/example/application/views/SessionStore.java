bc081bbae80562c40fc4040f0a92872362ada6f8
TatuWorkLenovo\Tatu <tatu@vaadin.com>
2024-01-25 11:03:15 +0200

    Add missing annotation

--- src/main/java/com/example/application/views/SessionStore.java
+++ src/main/java/com/example/application/views/SessionStore.java
@@ -1,9 +1,12 @@
 package com.example.application.views;
 
+import org.springframework.stereotype.Component;
+
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
 
 // Session scoped bean for just demonstrating
 @VaadinSessionScope
+@Component
 public class SessionStore {
 
     private String attribute;
